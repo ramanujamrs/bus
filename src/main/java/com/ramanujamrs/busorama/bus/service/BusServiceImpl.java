50974dba3bb222d6d0e69e364c44c17146106a79
Ramanujam Srinivasan <ramanujam.rs@gmail.com>
2020-06-22 21:47:39 -0500

    Added logging in ServiceImpl

--- src/main/java/com/ramanujamrs/busorama/bus/service/BusServiceImpl.java
+++ src/main/java/com/ramanujamrs/busorama/bus/service/BusServiceImpl.java
@@ -1,5 +1,6 @@
 package com.ramanujamrs.busorama.bus.service;
 
+import com.ramanujamrs.busorama.bus.controller.BusController;
 import com.ramanujamrs.busorama.bus.dao.BusRepository;
 import com.ramanujamrs.busorama.bus.model.Bus;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -7,14 +8,20 @@ import org.springframework.stereotype.Service;
 
 import java.util.List;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 @Service("busService")
 public class BusServiceImpl implements BusService
 {
     @Autowired
     private BusRepository busRepository;
+    private static final Logger LOGGER=LoggerFactory.getLogger(BusServiceImpl.class);
 
     @Override
-    public List<Bus> getBus() {
+    public List<Bus> getBus()
+    {
+        LOGGER.info("Execution");
         List<Bus> busList = busRepository.findAll();
         return busList;
     }
